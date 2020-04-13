6046f23dbe2023500fe6ef4d25369ea66d7de54a
Reyhanfernanda <Reyhanfernanda77@gmail.com>
2020-04-13 15:15:32 +0700

    online

---
+++ Recyclerview/app/src/main/java/android/com/reyhan/apk/MainActivity.kt
@@ -0,0 +1,29 @@
+package android.com.reyhan.apk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val list = ArrayList<Users>()
+    val listUsers = arrayOf(
+        "reyhan",
+        "oioi"
+    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mRecyclerView.setHasFixedSize(true)
+        mRecyclerView.layoutManager = LinearLayoutManager ( this)
+        for (i in 0 until listUsers.size){
+            list.add(Users(listUsers.get(i)))
+            if(listUsers.size - 1 == i){
+                val adapter = Adapter(list)
+                adapter.notifyDataSetChanged()
+                mRecyclerView.adapter = adapter
+            }
+        }
+    }
+}
---
+++ Recyclerview/app/src/main/java/android/com/reyhan/apk/Users.kt
@@ -0,0 +1,3 @@
+package android.com.reyhan.apk
+
+data class Users (val name: String?)
